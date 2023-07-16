301b1b81221177c7a8f46ce8a5343da61dbaaa36
Bismah <bismahmalik22@gmail.com>
2023-07-16 18:04:59 +0500

    Comm

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const routes = require('./routes/route')
+const app = express()
+const axios = require('axios');
+const Dictionary = require('./models/DictionaryModel')
+const path = require('path')
+const port = 3000
+const mongoose = require("mongoose")
+const uri = "mongodb+srv://211354:Rx1yppNoqR7ljL2S@cluster0.3wcc5i2.mongodb.net/FetchedDictionary?retryWrites=true&w=majority"
+
+mongoose.connect(uri, {
+  serverSelectionTimeoutMS: 30000,
+  socketTimeoutMS: 30000,
+});
+
+app.use('/', routes);
+
+app.get('/', (req, res) => {
+  return res.sendFile(path.join(__dirname, 'publics', 'index01.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'publics'), {
+  extensions: ['html', 'css', 'js'] // Add 'css' to the list of allowed extensions
+}));
+
+app.use(express.json())
+mongoose.connect(uri)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:3000`);
+    });
+  })
